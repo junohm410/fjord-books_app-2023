e699ce8401d03294df3c8e26ca095536439d72ba
junohm410 <junohm410@gmail.com>
2023-08-24 21:15:57 +0900

    ユーザー一覧・詳細のビュー・コントローラと、それらへのリンクを設置

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:index, :show]
+
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "books#index"
