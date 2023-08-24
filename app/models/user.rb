d9b538aa49b10da6744323e6010dbc5d1ccca087
junohm410 <junohm410@gmail.com>
2023-08-24 22:02:07 +0900

    郵便番号と自己紹介にバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :zip_code, numericality: { only_integer: true }, length: { is: 7 }, allow_nil: true
+  validates :profile, length: { maximum: 200 }
 end
