f1764f421cea221030557890d7f13dbf0e32df01
junohm410 <junohm410@gmail.com>
2023-08-27 23:22:19 +0900

    投稿者によるコメント削除機能を実装

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -7,6 +7,13 @@ class Reports::CommentsController < ApplicationController
     redirect_to report_path(@report)
   end
 
+  def destroy
+    @report = Report.find(params[:report_id])
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
+    redirect_to report_path(@report), status: :see_other
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)
