df1919758d535272d8a072f87c788a323d6e0adc
junohm410 <junohm410@gmail.com>
2023-08-28 17:10:08 +0900

    日報・本が削除されるとコメントも削除されるように / 退会したユーザーのデータの扱いについて修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_27_085745) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_053212) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -86,6 +86,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_27_085745) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "users"
-  add_foreign_key "reports", "users"
 end
