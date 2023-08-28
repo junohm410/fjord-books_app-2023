c440d5aeacada116b79f1491df5034cf48d3ef90
junohm410 <junohm410@gmail.com>
2023-08-28 11:57:55 +0900

    本へのコメント機能を追加

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,2 +1,21 @@
-class Reports::CommentsController < ApplicationController
+class Books::CommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = @book.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to report_path(@book)
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
+    redirect_to report_path(@book), status: :see_other
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
