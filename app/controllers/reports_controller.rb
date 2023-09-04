bb4a4b3f4d780cccaa7903ee62376665908d9b97
junohm410 <junohm410@gmail.com>
2023-09-04 11:51:04 +0900

    コメントの新規オブジェクトを本・日報のコントローラで取得せずビューで取得する

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,6 @@ class ReportsController < ApplicationController
   # GET /reports/1 or /reports/1.json
   def show
     @report = Report.find(params[:id])
-    @comment = Comment.new
   end
 
   # GET /reports/new
