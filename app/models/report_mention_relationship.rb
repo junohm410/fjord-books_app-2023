8140dc8f460b0fc046d02437baea3a2de611afc7
junohm410 <junohm410@gmail.com>
2023-09-04 22:00:50 +0900

    同じ言及元/先の組み合わせが登録されないようモデルにもバリデーションを追加 / 翻訳の追加

--- app/models/report_mention_relationship.rb
+++ app/models/report_mention_relationship.rb
@@ -1,4 +1,6 @@
 class ReportMentionRelationship < ApplicationRecord
   belongs_to :mentioning_report, class_name: "Report" 
   belongs_to :mentioned_report, class_name: "Report"
+
+  validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id }
 end
