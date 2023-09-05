0aab85e4d47d4d38d16b3a6b0d1576fd7ba46989
junohm410 <junohm410@gmail.com>
2023-09-05 18:39:30 +0900

    言及を表示する際のN+1問題のケア

--- app/models/report_mention_relationship.rb
+++ app/models/report_mention_relationship.rb
@@ -3,5 +3,5 @@ class ReportMentionRelationship < ApplicationRecord
   belongs_to :mentioned_report, class_name: "Report"
 
   validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id }
-  validates :mentioned_report_id, numericality: { less_than: Report.last.id }
+  validates :mentioned_report_id, numericality: { less_than_or_equal_to: Report.last.id }
 end
