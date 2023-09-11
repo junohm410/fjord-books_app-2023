d13e3680f1f9518db7072eab42922d9a8b386dee
junohm410 <junohm410@gmail.com>
2023-09-11 18:18:33 +0900

    Userモデルテストを追加 / 日報のシステムテストをscaffold時の内容に沿って動くように修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User#name_or_email should return name if user has no name" do
+    assert_equal 'alice@example.com', users(:alice).name_or_email
+  end
+
+  test "User#name_or_email should return name if user has name" do
+    assert_equal 'bob', users(:bob).name_or_email
+  end
 end
